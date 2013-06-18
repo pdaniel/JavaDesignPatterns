50964a21304600f9498d69a3c3c18f8c7b4b0e07
pdaniel <pdaniel2005@gmail.com>
2013-06-18 12:11:47 +0300

    singleton pattern multiple versions

---
+++ design-patterns/src/test/java/com/ondevel/design/patterns/creational/SingletonTest.java
@@ -0,0 +1,15 @@
+package com.ondevel.design.patterns.creational;
+
+import org.junit.Test;
+
+/**
+ * @author: danielpo
+ * Date: 6/18/13
+ * Time: 12:11 PM
+ */
+public class SingletonTest {
+    @Test
+    public void testSingleton(){
+
+    }
+}
